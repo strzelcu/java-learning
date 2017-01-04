902e51d3151de9a736aeb3c6d6cb03e043251255
Strzelcu <strzelecki.t@gmail.com>
2017-01-04 20:04:54 +0100

    Wykonane część ćwiczeń z rozdziału 4.

---
+++ src/rozdzial4/test4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rozdzial4;
+
+/**
+ *
+ * @author Strzelcu
+ */
+public class test4 {
+    public static void main(String[] args) {
+        
+        int range = 100;
+        boolean condition;
+        
+        for(int i = 1; i < range + 1; i++) {
+            
+            condition = true;
+            
+            for(int j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    condition = false;
+                }
+            }
+            
+            if(condition) {
+                System.out.println(i + " jest liczbą pierwszą");
+            }
+        }
+    }
+}
