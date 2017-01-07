9aa874e96169df3801607b090a33fc8cc3db67b1
Strzelcu <strzelecki.t@gmail.com>
2017-01-07 19:33:00 +0100

    Wykonane kolejne ćwiczenia z rozdziału 5.

---
+++ src/rozdzial5/test14.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rozdzial5;
+
+/**
+ *
+ * @author Strzelcu
+ */
+
+class Foto {
+    static String fotografia1 = "Fotografia Ślubna";
+    static String fotografia2;
+    
+    static {
+        fotografia2 = "Fotografia rodzinna";
+    }
+    
+    public static void print() {
+        System.out.println("Fotografia1 = " + fotografia1);
+        System.out.println("Fotografia2 = " + fotografia2);
+    }
+    
+}
+
+public class test14 {
+    public static void main(String[] args) {
+        Foto.print();
+    }
+}
